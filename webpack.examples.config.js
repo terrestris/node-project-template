a5c5eb87a8a47b5c1307fd22157b43acb6d54e53
Marc Jansen <jansen@terrestris.de>
2017-07-04 14:18:39 +0200

    Streamline webpack config

--- webpack.examples.config.js
+++ webpack.examples.config.js
@@ -1,27 +1,9 @@
-const path = require('path');
+const baseConfig = require('./webpack.config.js');
 
-module.exports = {
-  devtool: 'source-map',
-  entry: {
-    map: './examples/mapBuilder.js',
-  },
+baseConfig.devServer = {
+  inline: true,
+  host: '0.0.0.0',
+  port: 8080
+},
 
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js',
-  },
-
-  devServer: {
-    inline: true,
-    host: '0.0.0.0',
-    port: 8080
-  },
-
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  }
-};
+module.exports = baseConfig;
