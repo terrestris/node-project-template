b7a6ba65b1b0879e1bfafd4ef86534e26e8b9acc
Marc Jansen <jansen@terrestris.de>
2017-07-26 16:15:29 +0200

    Add basic API docs

--- src/Map/MapBuilder.js
+++ src/Map/MapBuilder.js
@@ -4,16 +4,36 @@ import TileLayer from 'ol/layer/tile';
 import XYZ from 'ol/source/xyz';
 
 /**
- * The map module.
- * @module Sol/map
+ * @module Sol
+ */
+
+/**
+ * This is the MapBuilder class.
+ *
+ * @class
  */
 export class MapBuilder {
+
+  /**
+   * Constructs a mapbuilder instance.
+   *
+   * @param {String|Element} [target] The optional target where the map will be
+   *     rendered in.
+   * @param {Array<Number>} [center] The optional center of the map.
+   * @param {Number} [zoom] The optional initial zoom of the map.
+   * @constructs
+   */
   constructor(target, center, zoom) {
     this.target = target || 'map';
     this.center = center || [0, 0];
     this.zoom = zoom || 0;
   }
 
+  /**
+   * Creates a map.
+   *
+   * @return {Map} The generated map.
+   */
   createMap() {
     let map = new Map({
       target: this.target,
